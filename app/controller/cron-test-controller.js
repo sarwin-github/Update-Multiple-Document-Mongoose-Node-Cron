803529c82e0e68c16079b287cfc667bff15c3c95
Sherwin Dapito <dapito.sherwin@yahoo.com>
2017-07-05 12:32:35 +0800

    Added node-cron task

--- app/controller/cron-test-controller.js
+++ app/controller/cron-test-controller.js
@@ -30,8 +30,7 @@ let createProduct = (name) => {
 // Get list of products
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 module.exports.getProducts = (req, res) => {
-	let query = Product.find({}).sort('dateCreated');
-
+	let query = Product.find({}).sort('dateCreated').select({'__v': 0 });
 	query.exec((err, products) => {
 		if(err) return res.status(500).json({
 			success:false, 
@@ -47,4 +46,53 @@ module.exports.getProducts = (req, res) => {
 				message:'Successfully fetched a list of products', 
 					products: products })
 	});	
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Get list of products thats less than the current date
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+module.exports.getProductLessDate = (req, res) => {
+	let currentDate = new Date();
+	let query = Product.find({ dateCreated: {$lt: currentDate}}).select({ 'status':0, '__v': 0 });
+	query.exec((err, products) => {
+		if(err) return res.status(500).json({
+			success:false, 
+				message: 'Something went wrong.', 
+					error: err });
+
+		if(!products) return res.status(404).json({
+			success:false, 
+				message: 'Product does not exist.' });
+
+		res.status(200).json({
+			success:true, 
+				message:'Successfully fetched a list of products', 
+					currentDate: currentDate,
+						products: products })
+	});	
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Update product description every 10 seconds
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+module.exports.updateProductSchedule = (req, res) => {
+	cron.schedule('*/10 * * * * *', function(){
+		let productArray = ['Bacon', 'Apple', 'Fork', 'Beans'];
+		productArray.map(product => updateProduct(product));
+	});
+};
+
+let updateProduct = (name) => {
+	let randomNumber = Math.floor(Math.random() * (999 - 1 + 1));
+	let query = Product.findOne({name: name}).select({ 'name': 1 });
+	query.exec((err, product) => {
+		if(err) throw err;
+		if(product){
+			product.description = `Random description generate every 10 seconds: ${randomNumber}`;
+			product.save(err =>{
+				if(err) throw err;
+				console.log(`Successfully updated random description for: ${product.name}\n ${product.description}\n`);	
+			});
+		}
+	});
 };
--- app/route/cron-test-route.js
+++ app/route/cron-test-route.js
@@ -5,5 +5,7 @@ const cronController = require('../controller/cron-test-controller');
 
 router.route('/product').get(cronController.getProducts);
 router.route('/product/create').post(cronController.createProducts);
+router.route('/product/date-created').get(cronController.getProductLessDate);
+router.route('/product/scheduled-update').put(cronController.updateProductSchedule);
 
 module.exports = router;
